64aafd21c0399894f9f6053a105063fcda2b84ca
Sam.S <samjesus241@gmail.com>
2018-07-26 14:37:11 +0530

    Create twoarrays.py

---
+++ twoarrays.py
@@ -0,0 +1,11 @@
+a=int(input())
+c=int(input())
+b=[]
+d=[]
+for a in range(a):
+    b.append(int(input()))
+for c in range(c):
+    d.append(int(input()))
+for e in d:
+    if e in b:
+        print("YES")
